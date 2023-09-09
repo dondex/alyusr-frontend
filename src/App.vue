d3318b716c1237fa9d0a5dcbd15119cc9bb58847
dex <dex.chavez@yahoo.com>
2023-09-09 10:25:19 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <body id="top">
+        <Header />
+        <RouterView/>
+        <Footer />
+    </body>
+
+</template>
+
+<script setup>
+import {  RouterView } from 'vue-router'
+import Header from './components/Layouts/Header.vue';
+import Footer from './components/Layouts/Footer.vue';
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <Banner />
+      <Features />
+      <AboutSection />
+      <CtaSection />
+      <ServiceSection />
+      <Appointment />
+      <Testimonial />
+    </div>
+</template>
+
+<script setup>
+import Banner from '../components/Banner.vue';
+import Features from '../components/Features.vue'
+import AboutSection from '../components/AboutSection.vue';
+import CtaSection from '../components/CtaSection.vue';
+import ServiceSection from '../components/ServiceSection.vue';
+import Appointment from '../components/Appointment.vue';
+import Testimonial from '../components/Testimonial.vue';
+</script>
